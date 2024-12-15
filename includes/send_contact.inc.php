8785060b3936221d9c991934bd252a290f26ffbe
taoista <taoista-@hotmail.com>
2024-12-15 07:04:11 -0300

    cambios de proyectos

---
+++ includes/send_contact.inc.php
@@ -0,0 +1,94 @@
+<?php
+// Cargar el autoload de Composer
+require '../vendor/autoload.php';
+
+// Usar la clase PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// ? emails 
+
+$emails_admin = [
+    'redes@cavaspaces.cl',
+    'contacto@cavaspaces.cl'
+];
+
+$name = strtolower($_POST['name']);
+$email_client = strtolower(trim($_POST['email']));
+$message = $_POST['message'];
+
+
+if($name == "" OR $email_client == "" OR $message == ""){
+    return 'error';
+}
+// ? validar el email
+
+for ($i=0; $i < count($emails_admin) ; $i++) { 
+    $mail_admin = $emails_admin[$i];
+    send_email($mail_admin, $name, $message);
+
+}
+
+send_email($email_client, $name, $message);
+
+
+
+
+
+// * enviando el email
+function send_email($email, $name, $message){
+    try {
+        $mail = new PHPMailer(true);
+    
+        // Configuración del servidor SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';         // Servidor SMTP
+        $mail->SMTPAuth = true;
+        $mail->Username = 'ef115d1ba87f2d'; // Usuario SMTP
+        $mail->Password = '76003d28cae728';         // Contraseña SMTP
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 2525;                         // Puerto SMTP
+    
+        // Configurar el remitente y el destinatario
+        $mail->setFrom('no-reply@cavasspaces.cl', 'no-reply');
+        $mail->addAddress($email, $name);
+    
+        $mail->CharSet = 'UTF-8';
+        // Cargar la plantilla HTML
+        $template = file_get_contents('./email_template.html');
+
+        // Reemplazar las variables dinámicas en la plantilla
+        $variables = [
+            '{{name}}' => $name,
+            '{{date}}' => date('d-m-Y'),
+            '{{message}}' => $message,
+            '{{year}}' => date('Y'),
+        ];
+        foreach ($variables as $key => $value) {
+            $template = str_replace($key, $value, $template);
+        }
+
+        // Contenido del correo
+        $mail->isHTML(true);
+        $mail->Subject = 'Bienvenido a TuEmpresa';
+        $mail->Body = $template;
+    
+        // Enviar correo
+        $mail->send();
+        // echo 'El mensaje se ha enviado correctamente';
+    } catch (Exception $e) {
+        // echo "Error al enviar el correo: {$mail->ErrorInfo}";
+    }
+}
+
+
+function validateEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+
+
+echo 'success';
+
+
+?>
---
+++ js/contact.js
@@ -0,0 +1,59 @@
+
+
+
+
+document.querySelector(".btn-send-contact").addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    const btn_send = document.querySelector("#cont-send");
+    const btn_sending = document.querySelector("#cont-sending");
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    if(name == "" || email == "" || message == ""){
+        Swal.fire('Error!','Debe llenar todos los datos para continuar','error');
+        return false;
+    }
+
+
+    const parameters = {
+        name: name,
+        email: email,
+        message: message
+    }
+
+    new Promise((resolve, reject) => {
+        $.ajax({
+            data: parameters,
+            url: "./includes/send_contact.inc.php",
+            type: "POST",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            beforeSend:function(){
+                btn_send.style.display = 'none';
+                btn_sending.style.display = 'block';
+            },
+            success:function(response){
+                resolve(response);
+            }
+        })
+    }).then((res) => {
+        if(res == 'success'){
+            Swal.fire('Error!','Do you want to continue','success');
+            document.querySelector("#name").value = '';
+            document.querySelector("#email").value = '';
+            document.querySelector("#message").value = '';
+        }else{
+            Swal.fire('Error!','Error intente mas tarde','error');
+        }
+        btn_send.style.display = 'block';
+        btn_sending.style.display = 'none';
+    })
+
+
+
+
+})
